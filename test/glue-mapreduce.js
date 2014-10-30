2675251b432850bcceb55f920cec023565ba5ffc
airtoxin <tahirimaka6@gmail.com>
2014-10-30 18:29:48 +0900

    Implement mapper and reducer

--- test/glue-mapreduce.js
+++ test/glue-mapreduce.js
@@ -1,15 +1,34 @@
-var assert = require( 'assert' );
+var _       = require( 'lodash' ),
+    assert  = require( 'assert' ),
+    expect  = require( 'chai' ).expect,
+    path    = require( 'path' ),
+    rootDir = path.join( '.', '..' );
+
+var NotImplementError = require( rootDir, 'lib', 'errors' ).NotImplementError;
+var GlueMapReduce = require( path.join( rootDir, 'lib', 'glue-mapreduce' ) );
 
 describe( 'GlueMapReduce', function () {
-	describe( 'mapper', function () {
-		// body...
-	} );
+    var mr = null;
+    beforeEach( function ( done ) {
+        mr = new GlueMapReduce();
+        done();
+    } );
 
-	describe( 'reducer', function () {
-		// body...
-	} );
+    describe( 'mapper', function () {
+        it( 'Not Implemented', function ( done ) {
+            expect( mr.mapper ).to.throw( NotImplementError );
+            done();
+        } );
+    } );
 
-	describe( 'run', function () {
-		// body...
-	} );
+    describe( 'reducer', function () {
+        it( 'Not Implemented', function ( done ) {
+            expect( mr.mapper ).to.throw( NotImplementError );
+            done();
+        } );
+    } );
+
+    describe( 'run', function () {
+        // body...
+    } );
 } );
