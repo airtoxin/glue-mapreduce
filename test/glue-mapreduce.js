9943b06a225e3dc94778fd1840ae2470c7a645da
airtoxin <tahirimaka6@gmail.com>
2014-10-30 19:53:28 +0900

    Add shuffler

--- test/glue-mapreduce.js
+++ test/glue-mapreduce.js
@@ -82,4 +82,69 @@ describe( 'GlueMapReduce', function () {
             mr.run();
         } );
     } );
+
+    describe( 'shuffler', function () {
+        it( 'Same key must be merged', function ( done ) {
+            var data = [
+                { k: 'hoge', v: 1 },
+                { k: 'hoge', v: 2 }
+            ];
+            mr.shuffler( data, function ( error, result ) {
+                var expect = [ { hoge: [ 1, 2 ] } ];
+                assert.deepEqual( result, expect );
+                done();
+            } );
+        } );
+
+        it( 'Defferent key must be splited', function ( done ) {
+            var data = [
+                { k: 'hoge', v: 1 },
+                { k: 'fuga', v: 2 }
+            ];
+            mr.shuffler( data, function ( error, result ) {
+                var expect = [
+                    { hoge: [ 1 ] },
+                    { fuga: [ 2 ] }
+                ];
+                assert.deepEqual( result, expect );
+                done();
+            } );
+        } );
+
+        it( 'Not objected data', function ( done ) {
+            var data = [
+                'aaaaaaaaaaaaa',
+                { k: 'hoge', v: 2 }
+            ];
+            mr.shuffler( data, function ( error, result ) {
+                var expect = [ { hoge: [ 2 ] } ];
+                assert.deepEqual( result, expect );
+                done();
+            } );
+        } );
+
+        it( 'Not have k field should return undefined key', function ( done ) {
+            var data = [
+                { hoge: 'a', v: 2 },
+                { fuga: 'b', v: 4 }
+            ];
+            mr.shuffler( data, function ( error, result ) {
+                var expect = [ { 'undefined': [ 2, 4 ] } ];
+                assert.deepEqual( result, expect );
+                done();
+            } );
+        } );
+
+        it( 'Not have v field should return undefined value', function ( done ) {
+            var data = [
+                { k: 'a', hoge: 1 },
+                { k: 'a', hoge: 1 },
+            ];
+            mr.shuffler( data, function ( error, result ) {
+                var expect = [ { a: [ void(0), void(0) ] } ];
+                assert.deepEqual( result, expect );
+                done();
+            } );
+        } );
+    } );
 } );
